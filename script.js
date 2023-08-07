20e774bb11b2f0e2c407b55ae384fa85b1be484b
DaridaKo <112251804+DaridaKo@users.noreply.github.com>
2023-08-07 20:19:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.blog__items').slick({
+		arrows: true,
+		dots: true,
+		slidesToShow: 3,
+  		slidesToScroll: 3,
+  		autoplay: true,
+  		autoplaySpeed: 2000,
+	});
+});
